f3e7184012fc8fb479abcd2febcc971864b0a3f4
DenSpar <57890463+DenSpar@users.noreply.github.com>
2021-10-31 17:24:25 +0300

    наполнение и стили Модалки

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,14 +3,45 @@ import {isEmpty} from 'lodash';
 import {connect, useDispatch} from 'react-redux';
 import {hideBookInfo} from '@redux/actions';
 
+const ExpandedField = ({field}) => {
+    const [isShortListChoosen, setChoosenList] = useState(true);
+
+    const ShowHideFullList = ({isVisible, isShortValue, handler}) => {
+        return isVisible ? (
+            <span 
+            className="repStarsNumForList activeText"
+            onClick={() => handler()}>
+                ({isShortValue ? "показать" : "скрыть"} полный список)
+            </span>
+        ) : null
+    };
+
+    return (
+        <span
+        className="repStarsNumForList fontDefault">
+            {field.title}
+            {isShortListChoosen && field.numText} 
+            <ShowHideFullList 
+                isVisible={field.existLongValue}
+                isShortValue={isShortListChoosen}
+                handler={() => setChoosenList(!isShortListChoosen)}
+            />
+            {isShortListChoosen ? field.shortValue : field.fullValue}
+        </span>
+    )
+}
+
 const Modal = (state) => {
     const [isModal, setModal] = useState(false);
+    const [book, setBook] = useState({});
     const dispatch = useDispatch();
 
     useEffect (
         () => {
-            if (!isEmpty(state.modal)) setModal(true);
-            console.log(state.modal);
+            if (!isEmpty(state.modal)) {
+                setBook(state.modal)
+                setModal(true)
+            };
         },
         [state]
     );
@@ -22,16 +53,27 @@ const Modal = (state) => {
 
     return isModal && (
         <div className="modal flex justify-content_center flex-direction_column">
-            <div className="modal-body">
-                <h1>modal tittle</h1>
-                <span className="repStarsNumForList fontDefault">modal text</span>
-                <button onClick={() => closeModal()}>Close</button>
+            <div className="modal-body flex flex-direction_column justify-content_center align-items_center">
+                <div className="flex flex-direction_column justify-content_center align-items_center">
+                    <div className="contentContainer__modal flex">
+                        <img src={`http://covers.openlibrary.org/b/isbn/${book.isbnForCover}-M.jpg?default=false`} alt="битая картинка" className="cover_m" />
+                        <div className="contentContainer__modal flex flex-direction_column">
+                            <span className="modalTitleFont">{book.title}</span>
+                            <span className="repStarsNumForList fontDefault">
+                                {book.author.title}{book.author.shortValue}
+                            </span>
+                            <ExpandedField field={book.publishDate}/>
+                            <ExpandedField field={book.publisher}/>
+                            <ExpandedField field={book.isbn}/>
+                            <ExpandedField field={book.publishDate}/>
+                        </div>
+                    </div>
+                    <button onClick={() => closeModal()} className="modal__closeBtn">Close</button>
+                </div>
             </div>
         </div>
     )
 };
-
-// export default Modal;
 
 const mapStateToProps = (state) => {
     return state;
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -6,12 +6,10 @@ const initialState = {
 
 export function rootReducer(state = initialState, action) {
     switch(action.type) {
-        case 'NEW_SEARCH': return action.payload;
+        case 'NEW_SEARCH': return {...action.payload, modal: {}};
         case 'SHOW_BOOK_INFO': 
-        console.log({...state, modal: action.payload})
             return {...state, modal: action.payload};
         case 'HIDE_BOOK_INFO': 
-        console.log({...state, modal: action.payload})
             return {...state, modal: {}};
         default: return state;
     }
